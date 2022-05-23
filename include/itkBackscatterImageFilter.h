5eb056b0dd768da5fb0f7d2f2f8c14ffe3ef8021
Dženan Zukić <dzenan.zukic@kitware.com>
2022-05-23 13:59:33 +0200

    ENH: Adding BackscatterImageFilter

---
+++ include/itkBackscatterImageFilter.h
@@ -0,0 +1,143 @@
+/*=========================================================================
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         https://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkBackscatterImageFilter_h
+#define itkBackscatterImageFilter_h
+
+#include <vector>
+
+#include "itkImage.h"
+#include "itkImageToImageFilter.h"
+#include "itkImageRegionSplitterDirection.h"
+#include "itkMacro.h"
+#include "itkRGBPixel.h"
+#include "itkNumericTraits.h"
+#include "itkNumericTraitsRGBPixel.h"
+
+namespace itk
+{
+/** \class BackscatterImageFilter
+ * \brief Computes the estimated backscatter coefficient
+ *
+ * BackscatterImageFilter receives an input vector image representing
+ * RF spectra. One image direction represents the direction of an
+ * RF waveform emitted from an ultrasound probe. Remaining image
+ * directions may represent directions in physical space, such as
+ * where the probe contains multiple elements, or directions in
+ * time as multiple samples are captured in a sweep with the probe.
+ * Each pixel in the input image is a vector representing frequency
+ * components at bins based on the sampling frequency.
+ *
+ * BackscatterImageFilter generates an RGB output image with
+ * RGB channel intensities representing different backscatter estimates.
+ *
+ *
+ * \sa MaskedImageToHistogramFilter
+ * \sa Spectra1DImageFilter
+ * \sa VariableLengthVector
+ * \sa VectorImage
+ *
+ * \ingroup ITKImageStatistics
+ * \ingroup Ultrasound
+ */
+template <typename TInputImage,
+          typename TOutputImage = Image<RGBPixel<float>, TInputImage::ImageDimension>>
+class ITK_TEMPLATE_EXPORT BackscatterImageFilter : public ImageToImageFilter<TInputImage, TOutputImage>
+{
+public:
+  ITK_DISALLOW_COPY_AND_MOVE(BackscatterImageFilter);
+
+  /** Standard class type aliases. */
+  using Self = BackscatterImageFilter;
+  using Superclass = ImageToImageFilter<TInputImage, TOutputImage>;
+  using Pointer = SmartPointer<Self>;
+  using ConstPointer = SmartPointer<const Self>;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(BackscatterImageFilter, ImageToImageFilter);
+
+  /** Image type alias support */
+  static constexpr unsigned int ImageDimension = TInputImage::ImageDimension;
+
+  using InputImageType = TInputImage;
+  using InputImagePointer = typename TInputImage::Pointer;
+
+  using OutputImageType = TOutputImage;
+  using OutputImagePointer = typename TOutputImage::Pointer;
+
+  using InputRegionType = typename TInputImage::RegionType;
+  using InputSizeType = typename TInputImage::SizeType;
+  using InputIndexType = typename TInputImage::IndexType;
+  using InputPixelType = typename TInputImage::PixelType;
+
+  using OutputRegionType = typename TOutputImage::RegionType;
+  using OutputPixelType = typename TOutputImage::PixelType;
+
+  /** RF sampling frequency */
+  itkSetMacro(SamplingFrequencyMHz, float);
+  itkGetConstMacro(SamplingFrequencyMHz, float);
+
+  /** Low end of RF frequency band. Must be a positive value. */
+  itkSetMacro(FrequencyBandStartMHz, float);
+  itkGetConstMacro(FrequencyBandStartMHz, float);
+
+  /* High end of RF frequency band. Must be a positive value.*/
+  itkSetMacro(FrequencyBandEndMHz, float);
+  itkGetConstMacro(FrequencyBandEndMHz, float);
+
+  void
+  PrintSelf(std::ostream & os, Indent indent) const override;
+
+protected:
+  BackscatterImageFilter();
+  ~BackscatterImageFilter() override = default;
+
+  void
+  VerifyPreconditions() const override;
+
+  /** Allocate buffers and other initializations before threaded execution. */
+  void
+  BeforeThreadedGenerateData() override;
+
+  void
+  DynamicThreadedGenerateData(const OutputRegionType & regionForThread) override;
+
+  /** Compute backscatter for a pixel in the RF spectra vector image. */
+  OutputPixelType
+  ComputeBackscatter(const InputIndexType & index) const;
+
+private:
+  float m_SamplingFrequencyMHz = 0.0f;
+  float m_FrequencyBandStartMHz = 0.0f;
+  float m_FrequencyBandEndMHz = 0.0f;
+  float m_FrequencyDelta = 0.0f;
+
+  // Frequency band to consider for backscatter
+  unsigned int m_StartComponent = 0;
+  unsigned int m_EndComponent = 0;
+  unsigned int m_ConsideredComponents = 1;
+};
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#  include "itkBackscatterImageFilter.hxx"
+#endif
+
+#endif
---
+++ include/itkBackscatterImageFilter.hxx
@@ -0,0 +1,149 @@
+/*=========================================================================
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         https://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkBackscatterImageFilter_hxx
+#define itkBackscatterImageFilter_hxx
+
+#include <algorithm>
+#include <cmath>
+
+#include "itk_eigen.h"
+#include ITK_EIGEN(Dense)
+#include "itkMath.h"
+#include "itkImageScanlineConstIterator.h"
+
+namespace itk
+{
+
+template <typename TInputImage, typename TOutputImage>
+BackscatterImageFilter<TInputImage, TOutputImage>::BackscatterImageFilter()
+{
+  this->SetNumberOfRequiredInputs(1);
+}
+
+template <typename TInputImage, typename TOutputImage>
+void
+BackscatterImageFilter<TInputImage, TOutputImage>::VerifyPreconditions() const
+{
+  Superclass::VerifyPreconditions();
+
+  if (this->GetSamplingFrequencyMHz() < itk::Math::eps)
+  {
+    itkExceptionMacro("RF sampling frequency was not set!");
+  }
+}
+
+template <typename TInputImage, typename TOutputImage>
+void
+BackscatterImageFilter<TInputImage, TOutputImage>::BeforeThreadedGenerateData()
+{
+  Superclass::BeforeThreadedGenerateData();
+
+  // Initialize metric image
+  this->GetOutput()->Allocate();
+
+  // Initialize iVars used in ComputeBackscatter()
+  float nyquistFrequency = m_SamplingFrequencyMHz / 2;
+  float numComponents = this->GetInput()->GetNumberOfComponentsPerPixel();
+  m_FrequencyDelta = nyquistFrequency / numComponents;
+  m_StartComponent = m_FrequencyBandStartMHz / m_FrequencyDelta;
+  m_EndComponent = m_FrequencyBandEndMHz / m_FrequencyDelta;
+  if (m_EndComponent == 0) // If m_FrequencyBandEndMHz is not set
+  {
+    m_EndComponent = numComponents - 1; // Use all components
+  }
+  m_ConsideredComponents = m_EndComponent - m_StartComponent + 1;
+}
+
+template <typename TInputImage, typename TOutputImage>
+void
+BackscatterImageFilter<TInputImage, TOutputImage>::DynamicThreadedGenerateData(
+  const OutputRegionType & regionForThread)
+{
+  if (regionForThread.GetNumberOfPixels() == 0)
+  {
+    return;
+  }
+
+  const InputImageType * input = this->GetInput();
+  OutputImageType *      output = this->GetOutput();
+
+  ImageScanlineConstIterator<TInputImage> it(input, regionForThread);
+  it.GoToBegin();
+
+  // do the work
+  while (!it.IsAtEnd())
+  {
+    while (!it.IsAtEndOfLine())
+    {
+      InputIndexType index = it.GetIndex();
+
+      OutputPixelType estimatedBackscatter = ComputeBackscatter(index);
+
+      output->SetPixel(index, estimatedBackscatter);
+      ++it;
+    }
+    it.NextLine();
+  }
+};
+
+template <typename TInputImage, typename TOutputImage>
+typename BackscatterImageFilter<TInputImage, TOutputImage>::OutputPixelType
+BackscatterImageFilter<TInputImage, TOutputImage>::ComputeBackscatter(const InputIndexType & index) const
+{
+  using ScalarType = typename NumericTraits<OutputPixelType>::ValueType;
+
+  // Get RF spectra frequency bins at start and end pixel positions
+  auto           input = this->GetInput();
+  InputPixelType sample = input->GetPixel(index);
+  ScalarType     sum = 0;
+
+  Eigen::Matrix<float, Eigen::Dynamic, 2> A(m_ConsideredComponents, 2);
+  Eigen::Matrix<float, Eigen::Dynamic, 1> b(m_ConsideredComponents);
+  for (unsigned i = 0; i < m_ConsideredComponents; i++)
+  {
+    A(i, 0) = 1;
+    A(i, 1) = (1 + i + m_StartComponent) * m_FrequencyDelta; // x_i = frequency
+    b(i) = sample[i + m_StartComponent];                     // y_i = intensity
+    sum += sample[i + m_StartComponent];
+  }
+
+  // from https://eigen.tuxfamily.org/dox/group__LeastSquares.html
+  Eigen::Matrix<float, 1, 2> lineFit = A.householderQr().solve(b);
+  ScalarType                 frequencySlope = -lineFit(1);
+  ScalarType                 frequencyIntercept = lineFit(0);
+
+  OutputPixelType result = NumericTraits<OutputPixelType>::Zero;
+  result[0] = sum / m_ConsideredComponents;
+  result[1] = frequencySlope;
+  result[2] = frequencyIntercept;
+
+  return result;
+}
+
+template <typename TInputImage, typename TOutputImage>
+void
+BackscatterImageFilter<TInputImage, TOutputImage>::PrintSelf(std::ostream & os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+
+  os << indent << "Sampling frequency (MHz): " << this->GetSamplingFrequencyMHz() << std::endl;
+  os << indent << "Frequency band: [" << this->GetFrequencyBandStartMHz() << "," << this->GetFrequencyBandEndMHz()
+     << "]" << std::endl;
+}
+} // end namespace itk
+#endif // itkBackscatterImageFilter_hxx
